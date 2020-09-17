2217476ff822a51010cd32d932458559b78bd58a
Itamar Tati <itamar.tati1994@gmail.com>
2020-09-17 02:59:00 +0100

    added functions and form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function encriptMessage(string, rotation){
+  let encriptedMessage = ''
+  for(let index = 0; index < string.length; index++){
+      let letter = string[index]
+      let rotatedLetter = findEncriptedIndexOfLetter(letter, rotation)
+      encriptedMessage += rotatedLetter
+  }
+  return encriptedMessage
 }
 
-export default App;
+function findEncriptedIndexOfLetter(character, rotationNumber){
+  let isCapital = false 
+  if (/[^A-Za-z]/.test(character)) return character
+  if (character === character.toUpperCase()){
+      isCapital = true
+      character = character.toLowerCase()
+  } 
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  for(let index = 0; index < alphabet.length; index++){
+      let letter = alphabet[index]
+      if (letter === character){
+          if(index + rotationNumber > 25) rotationNumber = rotationNumber - 26
+          return isCapital ? alphabet[index + rotationNumber].toUpperCase() : alphabet[index + rotationNumber]
+      }
+  }
+}
+
+export default () => {
+  const [message, setMessage] = useState('')
+  const [rotation, setRotation] = useState(13)
+
+  return (
+  <div>
+    <p>{encriptMessage(message, rotation)}</p>
+    <form>
+    <input value={message} onChange={e => setMessage(e.target.value)} />
+      <select onChange={e => setRotation(parseInt(e.target.value))}>
+        <option value= "11" >11</option>
+        <option value="12">12</option>
+        <option selected value="13">ROT-13</option>
+        <option value="14">14</option>
+      </select>
+    </form>
+  </div>)
+} 
+
+
+
